708d700a6837a462f81d6b20b46aa8f032cd16ea
Joshua <joshuadstarkey@gmail.com>
2018-10-02 12:52:33 -0700

    adding more instructions, some html strucutre

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,13 @@
-// Theme of the game is:
+// Theme of the game is: record labels
+
+// Need to use key events to trigger behavior
+// onkeyup
+// 	- (listener) capture the key that was pressed, store in variable
+//  	- onkeyup- trigger a decrease in the guesses
+//
+//
+//
+//
+//
+//
+//
