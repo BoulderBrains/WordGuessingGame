ac41909d1babc436e27877e4dfa17de0de0b0b5b
Joshua <joshuadstarkey@gmail.com>
2018-10-03 00:44:42 -0700

    Base variables in place, pseudo code in place for remainder of project.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,41 @@
 // Theme of the game is: Learning to Code
 
-
-//  	- onkeyup- trigger a decrease in the guesses
+// Setting up some base variables to be used later
+var wins = 0;
+var alreadyPlayed = [];
+var remainingTrys = 5;
 
 document.onkeyup = function (event) {
+
 	// Capture the key that was pressed, store in userInput variable
 	var userInput = event.key;
 
 	// List of possible words user has to guess from
 	var possibleWords = ["Function", "Array", "Method", "Truthy", "event", "options", "include"]
 
-	// Creating a wordBeingGuessed that is set to a random word out of the possible words index
+	// Pulling a random word out of the possibleWords array
 	var wordBeingGuessed = possibleWords[Math.floor(Math.random() * possibleWords.length)];
-	
-
 	
 	console.log(wordBeingGuessed);
 	console.log(userInput);
+	console.log(wins);
+	console.log(alreadyPlayed);
 };
+
+// TODO: 
+// - create place in HTML file for wordBeingGuessed to be displayed
+// - display wordBeingGuessed into HTML element but 
+// masked as '_ _ _ _ _' (thinking a for loop, going through 
+// the characters and changing them to "_" might do the trick)
+// - Create function that compares userInput each letter of wordBeingGuessed
+// IF userInput === wordBeingGuessed[i] update wordBeingGuessed[i] back from _ to userInput
+// else remainingTrys-- &
+// // add the userInput to the alreadyPlayed array
+// alreadyPlayed.push(userInput);
+
+// - create function that compares the wordBeingGuessed with remainingTrys
+// IF wordsBeingGuessed is completed (ie all "_ _ _" have been changed to "characters")
+// User wins the fucking game!!!
+// ELSE IF remainingTrys === 0 {
+// 	Sorry bub, you fucking lose
+// }
