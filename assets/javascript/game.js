72a8687130962f72037963f15d5d268e3ad9ab7e
Joshua <joshuadstarkey@gmail.com>
2018-10-02 18:32:33 -0700

    adding more psudo-code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,7 +5,10 @@
 // 	- (listener) capture the key that was pressed, store in variable
 //  	- onkeyup- trigger a decrease in the guesses
 //
-//
+// Next, we give JavaScript a function to execute when onkeyup event fires.
+// document.onkeyup = function(event) {
+// 	userText.textContent = event.key;
+//  };
 //
 //
 //
